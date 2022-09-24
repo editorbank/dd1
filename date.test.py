6f42ecef302671a60491d5383866dc6c5a7a540c
editorbank <>
2022-09-24 23:21:28 +0300

    add datetime

--- date.test.py
+++ date.test.py
@@ -8,4 +8,9 @@ assert('DATE_DD_MM_YYYY' not in detect('32.12.9999'))
 assert('DATE_DD_MM_YYYY' not in detect('00.00.0000'))
 assert('DATE_DD_MM_YYYY' in detect('31.12.9999'))
 assert('DATE_DD_MM_YYYY' in detect('01.01.0000'))
-assert('DATE_MM_DD_YYYY' in detect('01.01.0000'))
+
+assert('DATETIME_LIKE' in detect('31.12.9999 00.00.00'))
+assert('DATETIME_LIKE' in detect('9999-12-31T23:59:59'))
+assert('DATETIME_LIKE' in detect('99 11 11 23 59'))
+assert('DATETIME_LIKE' not in detect('99 11 11 23 60'))
+
--- dd1/dd1_date.py
+++ dd1/dd1_date.py
@@ -4,10 +4,9 @@ _YY = r"([0-9]{2})"
 _Y4 = r"([0-9]{4})"
 _MM = r"(0[1-9]|1[0-2])"
 _DD = r"(0[1-9]|[12][0-9]|3[01])"
-_S = r"[\.\/\-_ ]"
-__hh = r"([01][0-9]|2[0-3])"
-__mi = r"([0-5][0-9])"
-__se = r"([0-5][0-9])"
+_S = r"[\.\/\-_ :T]"
+_HH = r"([01][0-9]|2[0-3])"
+_59 = r"([0-5][0-9])"
 DD1_DATE_DDMMYY = f"^{_DD}{_MM}{_YY}$"
 DD1_DATE_YYMMDD = f"^{_YY}{_MM}{_DD}$"
 DD1_DATE_YYDDMM = f"^{_YY}{_DD}{_MM}$"
@@ -21,17 +20,17 @@ DD1_DATE_YYYY_MM_DD = f"^{_Y4}{_S}{_MM}{_S}{_DD}$"
 DD1_DATE_YYYY_DD_MM = f"^{_Y4}{_S}{_DD}{_S}{_MM}$"
 DD1_DATE_MM_DD_YYYY = f"^{_MM}{_S}{_DD}{_S}{_Y4}$"
 
-DD1_DATE_LIKE = ree(f"""
-  {DD1_DATE_DDMMYY}|
-  {DD1_DATE_YYMMDD}|
-  {DD1_DATE_YYDDMM}|
-  {DD1_DATE_MMDDYY}|
-  {DD1_DATE_DDMMYYYY}|
-  {DD1_DATE_YYYYMMDD}|
-  {DD1_DATE_YYYYDDMM}|
-  {DD1_DATE_MMDDYYYY}|
-  {DD1_DATE_DD_MM_YYYY}|
-  {DD1_DATE_YYYY_MM_DD}|
-  {DD1_DATE_YYYY_DD_MM}|
-  {DD1_DATE_MM_DD_YYYY}
-  """)
+_DATE_LIKE = ree(f"""
+  (
+    {_DD}{_MM}({_YY}|{_Y4})|
+    ({_YY}|{_Y4}){_MM}{_DD}|
+    ({_YY}|{_Y4}){_DD}{_MM}|
+    {_MM}{_DD}({_YY}|{_Y4})|
+    {_DD}{_S}{_MM}{_S}({_YY}|{_Y4})|
+    ({_YY}|{_Y4}){_S}{_MM}{_S}{_DD}|
+    ({_YY}|{_Y4}){_S}{_DD}{_S}{_MM}|
+    {_MM}{_S}{_DD}{_S}({_YY}|{_Y4})
+  )
+""")
+DD1_DATE_LIKE = ree(f"^{_DATE_LIKE}$")
+DD1_DATETIME_LIKE = ree(f"^{_DATE_LIKE}{_S}{_HH}{_S}{_59}({_S}{_59})?$")
