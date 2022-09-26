aa884abe16f1414f18baa5830e4072af5bbee928
editorbank <>
2022-09-26 12:33:53 +0300

    mody NUM_OCT_C

--- dd1/dd1_num_hex.py
+++ dd1/dd1_num_hex.py
@@ -5,5 +5,4 @@ _C_HEX_PREFIX = r"\\0x"
 
 DD1_NUM_HEX = f"^{_DIGIT}+$"
 DD1_NUM_HEX_SIG = f"^{_SIGN}?{_DIGIT}+$"
-DD1_C_HEX_BYTE = f"^{_C_HEX_PREFIX}{_DIGIT}{{2}}$"
-DD1_C_HEX_WORD = f"^{_C_HEX_PREFIX}{_DIGIT}{{4}}$"
+DD1_NUM_HEX_C = f"^{_C_HEX_PREFIX}{_DIGIT}{_DIGIT}$"
--- dd1/dd1_num_oct.py
+++ dd1/dd1_num_oct.py
@@ -1,8 +1,19 @@
 from .dd1_num_bin import _SIGN
 
 _DIGIT = "[0-7]"
-_C_OCT_PREFIX = r"\\0"
+_C_OCT_PREFIX = r"\\"
 
 DD1_NUM_OCT = f"^{_DIGIT}+$"
 DD1_NUM_OCT_SIG = f"^{_SIGN}?{_DIGIT}+$"
-DD1_NUM_OCT_C = f"^{_C_OCT_PREFIX}{_DIGIT}{{3}}$"
+DD1_NUM_OCT_C = f"""(?x) # восьмеричное число в синтаксисе языка Си
+  ^
+  {_C_OCT_PREFIX}
+  (
+    0{{1,3}}
+    |
+    0?{_DIGIT}{_DIGIT}?
+    |
+    [1-3]{_DIGIT}{_DIGIT}
+  )
+  $
+  """
