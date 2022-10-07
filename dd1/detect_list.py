856345f3ce8134fa8cb5f5f77a0cb98cbd9afe65
editorbank <>
2022-10-07 23:57:38 +0300

    refactoring

--- dd1/detect_list.py
+++ dd1/detect_list.py
@@ -1,17 +1,17 @@
-from .detect_str import alltags, detect_str
+from .detect_value import detect_value
 
 def set_intersection(a:set, b:set):
   return [ i for i in a if i in b ]
 
 def detect_list(values:list):
   if len(values)==1:
-    d1 = sorted(detect_str(values[0]))
+    d1 = sorted(detect_value(values[0]))
     return {"all": d1, "meets": []}
   else:
-    all = detect_str(values[0])
+    all = detect_value(values[0])
     meets = all
     for value in values[1:]:
-      d1 = detect_str(value)
+      d1 = detect_value(value)
       all = set_intersection(all, d1)
       meets.update(d1)
   return {"all": sorted(all), "meets": sorted([i for i in meets if i not in all])}
