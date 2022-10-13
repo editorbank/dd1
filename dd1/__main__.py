7a5707ee8582effe801e6385e8d9cf3db72736d8
editorbank <>
2022-10-13 23:08:20 +0300

    Файл строк в виде источника данных#22

--- dd1/__main__.py
+++ dd1/__main__.py
@@ -1,30 +1,37 @@
 from sys import argv
-from . import detect_value, detect_list, detect_csv, alltags, escaped_options
+
+from . import detect_list, detect_csv, alltags, escaped_options, read_lines_from_file
 import json
 
 def dumps(obj)->str:
   return json.dumps(obj, indent=1, ensure_ascii=False)
+
+def dict_from_strings_list(argv)->dict:
+  args = {}
+  for v in argv:
+    vs = v.split("=",maxsplit=2)
+    args[vs[0]]=vs[1].encode().decode('unicode-escape') if vs[0] in (escaped_options) else vs[1]
+  return args
+
+def help():
+    print("""Data Detector v.1
+    Use: python -m dd1 <function> [<param1> [<param2> [...]]]
+    Functions: tags|arg|line|csv
+    """)  
 
 if __name__ == "__main__":
   if len(argv) > 1:
     func = argv[1]
     if func == "tags":
       print(dumps(list(sorted(alltags()))))
-    elif func == "value" and len(argv) > 2:
-      for i in range(2,len(argv)):
-        print(dumps(detect_value(argv[i])))
-    elif func == "list" and len(argv) > 2:
+    elif func == "values" and len(argv) > 2:
       print(dumps(detect_list(argv[2:])))
+    elif func == "lines" and len(argv) > 2:
+      print(dumps(detect_list(read_lines_from_file(**dict_from_strings_list(argv[2:])))))
     elif func == "csv" and len(argv) > 2:
-      args = {}
-      for v in argv[2:]:
-        vs = v.split("=",maxsplit=2)
-        args[vs[0]]=vs[1].encode().decode('unicode-escape') if vs[0] in (escaped_options) else vs[1]
-      print(dumps(detect_csv(**args)))
+      print(dumps(detect_csv(**dict_from_strings_list(argv[2:]))))
     else:
-      print("Undefined function!")  
+      print(f"Undefined function \"{func}\"!")
+      help()
   else:
-    print("""Data Detector v.1
-    Use: python -m dd1 <function> [<param1> [<param2> [...]]]
-    Functions: tags|value|list|csv
-    """)  
+    help()
--- dd1/ree.py
+++ dd1/ree.py
@@ -1,17 +1,20 @@
 import os
 import re
 
-def read_lines_from_file(filename:str):
-  this_module_path = os.path.dirname(__file__)
-  filepath = os.path.join(this_module_path,filename)
-  if os.path.isfile(filepath):
-    with open(filepath,'r') as f:
+def read_lines_from_file(filename:str, **kwargs):
+  if os.path.isfile(filename):
+    with open(filename,'r', **kwargs) as f:
       return [re.sub(r"(\n|\r\n|\r)$","",line) for line in f.readlines()]
   else:
     return []
 
+def module_path(filename:str):
+  this_module_path = os.path.dirname(__file__)
+  return os.path.join(this_module_path,filename)
+
+
 def res_file(filename:str)->str:
-  return "".join(read_lines_from_file(filename))
+  return "".join(read_lines_from_file(module_path(filename)))
 
 def res_str(res:str)->str:
   return "".join(re.split(r"\n|\r\n|\r",res))
@@ -20,7 +23,7 @@ def _clear_comments_and_external_spaces(lines:list):
   return [re.sub(r"^\s*[#].*$|(^\s+|\s+$)","",line) for line in lines]
 
 def ree_file(filename:str)->str:
-  return "".join(_clear_comments_and_external_spaces(read_lines_from_file(filename)))
+  return "".join(_clear_comments_and_external_spaces(read_lines_from_file(module_path(filename))))
 
 def ree(ree:str)->str:
   return "".join(_clear_comments_and_external_spaces(re.split(r"\n|\r\n|\r",ree)))
